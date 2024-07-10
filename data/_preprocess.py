fix visualization alpha scaling and preprocessing

data/_preprocess.py
@@ -5,29 +5,32 @@ import os
 
 import h5py
 import memmpy
-import numba
 import numpy as np
 
 import matplotlib.pyplot as plt
 
+from tqdm import tqdm
 
-def elementwise_preprocess(x, k=4):
+
+def elementwise_preprocess(x, k=1):
+
     for _ in range(k):
-        x = np.log(x + 1)
+        x = x + 1
+        x = np.log(x)
 
-    # x = np.nan_to_num(x, nan=0.0, posinf=0.0, neginf=0.0)
-    x = x * (2 / 0.74)
+    x = x * (2 / 5.3)
     x = x - 1
 
     return x
 
 
-def elementwise_preprocess_inv(x, k=4):
+def elementwise_preprocess_inv(x, k=1):
     x = x + 1
-    x = x * (0.8 / 2)
+    x = x * (5.3 / 2)
 
     for _ in range(k):
-        x = np.exp(x) - 1
+        x = np.exp(x)
+        x = x - 1
 
     return x
 
@@ -63,16 +66,24 @@ def preprocess(write_path, read_path):
 
     vector = memmpy.Vector()
 
-    for path in glob.glob(read_path):
+    for path in tqdm(glob.glob(read_path)):
         with h5py.File(path, "r") as f:
             showers = f["showers"]
             n = len(showers)  # type: ignore
 
-            for s, e in memmpy.batch_slices(n, 1024 * 32, False):
+            for s, e in memmpy.batch_slices(n, 1024 * 8, False):
                 x = showers[s:e]  # type: ignore
-                x = elementwise_preprocess(x)
+
+                x = np.maximum(x, 0)
+                x = np.log(1 + x) / (5.25 / 2)
+                x = np.nan_to_num(x, nan=0, posinf=0, neginf=0)
+
+                x = x - 1
+                x = np.clip(x, -1, 1)
+                x = x.astype(np.float32)
 
                 vector.extend(x)
+                break
 
             break
 
@@ -80,4 +91,4 @@ def preprocess(write_path, read_path):
     return preprocess(write_path, read_path)
 
 
-# preprocess("data/raw", "data/raw/*.h5")
+preprocess("raw", "raw/*.h5")

data/_visual.py
@@ -24,7 +24,11 @@ def linspace_with_gaps(start, stop, num, gap=0):
     return with_gap
 
 
-def plot_shower2(shower):
+def plot_shower(shower):
+    shower = shower - np.min(shower)
+    shower = shower / np.max(shower)
+    shower = explode(shower)
+
     r_dim, thet_dim, z_dim = shower.shape
 
     r_grid = linspace_with_gaps(0, 1, r_dim + 1)
@@ -47,19 +51,18 @@ def plot_shower2(shower):
     colors[..., :3] = rgb_colors
 
     alpha = shower
-    alpha = alpha - np.min(shower)
-    alpha = alpha / np.max(alpha)
     colors[..., 3] = alpha
 
     fig = plt.figure(figsize=(6, 6))
     ax: Axes3D = fig.add_subplot(111, projection="3d")  # type: ignore
 
     # Use ax.voxels to plot the voxels
+    print(np.mean(alpha))
     ax.voxels(  # type: ignore
         X,
         Y,
         Z,
-        shower > 0.05,
+        alpha > 0.01,
         facecolors=colors,
         shade=False,
     )
@@ -83,19 +86,13 @@ def plot_shower2(shower):
 
 def main():
     import random
+    from _preprocess import preprocess
 
-    import h5py
+    showers = preprocess("raw/", "raw/*.h5")
+    i = random.randint(0, len(showers))
+    shower = showers[i]
 
-    path = "data/raw/ddsim_mesh_Par04_gamma_500events_1GeV1TeV_GPSflat_edm4hep_12388001_part1.h5"
-
-    with h5py.File(path, "r") as f:
-        showers = f["showers"]  # type: ignore
-        i = random.randint(0, len(showers))  # type: ignore
-        shower: np.ndarray = showers[i]  # type: ignore
-
-    print(shower.shape)
-    shower = explode(shower)
-    plot_shower2(shower=shower)
+    plot_shower(shower[..., ::5])
     plt.show()
 
 